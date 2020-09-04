8c8baeb9aa109ca631509c2349011f8f875c1578
Gabriel Ribeiro <gribeiro@bionexo.com>
2020-09-04 08:27:10 -0300

    Include_rules_cancancan

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
      class ApiController < ApplicationController
       include Knock::Authenticable
+      include CanCan::ControllerAdditions
         #> Métodos globais
      end
   end
